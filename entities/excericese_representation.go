da2fb5f8e256a41e6867a8b0a6e278662bfa79f5
HustonMmmavr <huston.mavr@gmail.com>
2018-11-25 17:59:38 +0300

    added some fields to output model:

--- entities/excericese_representation.go
+++ entities/excericese_representation.go
@@ -4,18 +4,22 @@ type ExerciseRepresentation struct {
 	Id       uint     `json:"id"`
 	FileName string   `json:"file_name"`
 	Subject  string   `json:"subject"`
+	Answer   string   `json:"answer"`
 	Tags     []string `json:"tags"`
 	Level    int      `json:"level"`
 	Author   uint     `json:"author"`
+	IsBroken bool     `json:"is_broken"`
 }
 
 func NewExRepresentation(ex ExerciseEntity) ExerciseRepresentation {
 	return ExerciseRepresentation{
 		Id:       ex.Id,
 		FileName: ex.FileName,
+		Answer:   "",
 		Subject:  ex.Subject,
 		Tags:     ex.Tags,
 		Level:    ex.Level,
 		Author:   ex.AuthorId,
+		IsBroken: ex.IsBroken,
 	}
 }
