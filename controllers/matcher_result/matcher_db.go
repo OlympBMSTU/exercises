fef0915076d7598bfa4a0dc0cdabaa1e460f890a
HustonMmmavr <huston.mavr@gmail.com>
2018-11-26 02:33:17 +0300

    status mapping changed

--- controllers/matcher_result/matcher_db.go
+++ controllers/matcher_result/matcher_db.go
@@ -8,10 +8,12 @@ import (
 )
 
 var mapHttpDbStatuses = map[int]ResultInfo{
-	db.NO_ERROR:         NewResultInfo("Ok", http.StatusOK, statusOK), //, "OK"),
-	db.CREATED:          NewResultInfo("Created", http.StatusCreated, statusOK),
-	db.QUERY_ERROR:      NewResultInfo("Not found", http.StatusNotFound, statusError),
-	db.EMPTY_RESULT:     NewResultInfo("Not found", http.StatusNotFound, statusError),
+	db.NO_ERROR:     NewResultInfo("Ok", http.StatusOK, statusOK), //, "OK"),
+	db.CREATED:      NewResultInfo("Created", http.StatusCreated, statusOK),
+	db.QUERY_ERROR:  NewResultInfo("Ok", http.StatusOK, statusOK),
+	db.EMPTY_RESULT: NewResultInfo("Ok", http.StatusOK, statusOK),
+	// db.QUERY_ERROR:      NewResultInfo("Not found", http.StatusNotFound, statusError),
+	// db.EMPTY_RESULT:     NewResultInfo("Not found", http.StatusNotFound, statusError),
 	db.DB_CONN_ERROR:    NewResultInfo("Internal server error", http.StatusInternalServerError, statusError),
 	db.PARSE_ERROR:      NewResultInfo("Internal server error", http.StatusInternalServerError, statusError),
 	db.CONSTRAINT_ERROR: NewResultInfo("Connflict", http.StatusConflict, statusError),
